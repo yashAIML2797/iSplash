643f4eeee9d406d1e7ec8e867827b86fe44168c4
Yash Uttekar <yash.uttekar2797@icloud.com>
2023-09-13 17:49:31 +0530

    Initial Commit

---
+++ iSplash/iSplashApp.swift
@@ -0,0 +1,17 @@
+//
+//  iSplashApp.swift
+//  iSplash
+//
+//  Created by Yash Uttekar on 13/09/23.
+//
+
+import SwiftUI
+
+@main
+struct iSplashApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
