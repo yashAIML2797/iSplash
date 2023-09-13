5e4734a3b7cbc99a6b48184afae1dcce85631765
Yash Uttekar <yash.uttekar2797@icloud.com>
2023-09-13 22:37:39 +0530

    LazyVGrid

--- iSplash/ContentView.swift
+++ iSplash/ContentView.swift
@@ -12,30 +12,30 @@ struct ContentView: View {
     
     var body: some View {
         GeometryReader { geo in
-            VStack {
-                ScrollView {
-                    VStack {
-                        ForEach(photos, id: \.id) { photo in
-                            VStack {
-                                AsyncImage(url: URL(string: photo.urls.regular)) { image in
-                                    image
-                                        .resizable()
-                                        .scaledToFill()
-                                } placeholder: {
-                                    Image(systemName: "photo")
-                                        .resizable()
-                                        .scaledToFit()
-                                        .frame(width: geo.size.width * 0.5, height: 150)
-                                }
-                            }
-                            .frame(width: geo.size.width, height: 300)
-                            .clipped()
+            ScrollView {
+                LazyVGrid(columns: [GridItem(.flexible()),
+                                    GridItem(.flexible())],
+                          spacing: 5
+                ) {
+                    ForEach(photos, id: \.id) { photo in
+                        AsyncImage(url: URL(string: photo.urls.regular)) { image in
+                            image
+                                .resizable()
+                                .scaledToFill()
+                        } placeholder: {
+                            Image(systemName: "photo")
+                                .resizable()
+                                .scaledToFit()
                         }
+                        .frame(width: ((geo.size.width - 20) * 0.5),
+                               height: ((geo.size.width - 10) * 0.5)
+                        )
+                        .clipped()
                     }
                 }
+                .padding(.horizontal, 10)
             }
         }
-        .padding()
         .onAppear {
             APIService.shared.fetchPhotos(pageNumber: 1) { page in
                 self.photos = page.photos
