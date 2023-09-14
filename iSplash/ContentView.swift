7e54d8db9cdc57b356c39006f5ef2d27cf9a46d2
Yash Uttekar <yash.uttekar2797@icloud.com>
2023-09-14 14:53:42 +0530

    Loading more photos

--- iSplash/ContentView.swift
+++ iSplash/ContentView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var photos = [Photo]()
+    @State private var currentPage = 1
     
     var body: some View {
         GeometryReader { geo in
@@ -18,6 +19,10 @@ struct ContentView: View {
                           spacing: 5
                 ) {
                     ForEach(photos, id: \.id) { photo in
+                        
+                        let cellWidth =     (geo.size.width - 20) * 0.5
+                        let cellHeight =    (geo.size.width - 10) * 0.5
+                        
                         AsyncImage(url: URL(string: photo.urls.regular)) { image in
                             image
                                 .resizable()
@@ -26,19 +31,42 @@ struct ContentView: View {
                             Image(systemName: "photo")
                                 .resizable()
                                 .scaledToFit()
+                                .frame(width: cellWidth * 0.5, height: cellHeight * 0.5)
                         }
-                        .frame(width: ((geo.size.width - 20) * 0.5),
-                               height: ((geo.size.width - 10) * 0.5)
-                        )
+                        .frame(width: cellWidth, height: cellHeight)
+                        .cornerRadius(10)
                         .clipped()
                     }
                 }
                 .padding(.horizontal, 10)
+                
+                Button {
+                    currentPage += 1
+                    APIService.shared.fetchPhotos(pageNumber: currentPage) { page in
+                        for photo in page.photos {
+                            if !self.photos.contains(where: {$0.id == photo.id}) {
+                                self.photos.append(photo)
+                            }
+                        }
+                    }
+                } label: {
+                    Text("Load More")
+                        .font(.system(size: 18, weight: .semibold))
+                        .foregroundColor(.white)
+                        .padding(10)
+                        .background(.black)
+                        .cornerRadius(10)
+                }
+                .padding()
             }
         }
         .onAppear {
-            APIService.shared.fetchPhotos(pageNumber: 1) { page in
-                self.photos = page.photos
+            APIService.shared.fetchPhotos(pageNumber: currentPage) { page in
+                for photo in page.photos {
+                    if !self.photos.contains(where: {$0.id == photo.id}) {
+                        self.photos.append(photo)
+                    }
+                }
             }
         }
     }
